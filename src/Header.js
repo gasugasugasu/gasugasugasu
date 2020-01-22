360f93d1f336855a93c8654bf52f053962f5624c
菅原達也 <sugawaratatsuya@sugawaratatsuyanoMacBook-Pro.local>
2020-01-23 01:07:40 +0900

    header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className="header">
+        <div className="logo">
+          <p>Logo</p>
+        </div>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#">About</a></li>
+            <li><a href="#">contact</a></li>
+            <li><a href="#">Projects</a></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
